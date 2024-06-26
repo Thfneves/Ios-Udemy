43635539d35d38452cbef2161852bcbbb913a9e8
Thiago NeveS <140116209+Thfneves@users.noreply.github.com>
2024-06-26 17:53:31 -0300

    Add files via upload

---
+++ Quizz/Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Thiago Neves on 20/06/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Question {
+    let text: String
+        let answer: String
+    
+    init(q: String , a: String) {
+        text = q
+        answer = a
+    }
+}
